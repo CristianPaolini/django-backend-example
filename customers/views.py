9dcff42e2562612f346e7b8945769226bf942853
Cristian Paolini <cristianpaolini3@gmail.com>
2022-11-13 15:46:26 -0300

    surround get single endpoint with try-except

--- customers/views.py
+++ customers/views.py
@@ -1,4 +1,4 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, Http404
 from customers.models import Customer
 from customers.serializers import CustomerSerializer
 
@@ -8,7 +8,11 @@ def customers(request):
     serializer = CustomerSerializer(data, many=True)
     return JsonResponse({'customers': serializer.data})
 
+
 def customer(request, id):
-    data = Customer.objects.get(pk=id)
+    try:
+        data = Customer.objects.get(pk=id)
+    except Customer.DoesNotExist:
+        raise Http404('Customer does not exist')
     serializer = CustomerSerializer(data)
     return JsonResponse({'customer': serializer.data})
